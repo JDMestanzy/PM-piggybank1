601e2ae94bc3df8404504cbb7318ab0c0fe772bb
gregeva <evang0911@gmail.com>
2023-10-30 09:46:45 -0600

    made base funcitonality of the piggy bank allowing users to set a goal and then track progess as they add and delete amounts of money

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import indexPageView
+from .views import accountPageView
 
 urlpatterns = [
-    path("", indexPageView, name="index")
+    path("", indexPageView, name="index"),
+    path("account/<kid_name>", accountPageView, name="account")
 ]
